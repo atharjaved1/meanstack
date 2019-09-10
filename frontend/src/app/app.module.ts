11bfa887fdc82cc127be21fb2d7e487d7e8d941a
AtharJaved1 <atharjavedsb@gmail.com>
2019-09-10 21:10:25 +0500

    Loader Functionality Done

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,19 +6,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { ReservationFormComponent } from './reservation-form/reservation-form.component';
+ 
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     ReservationFormComponent
+      
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    NgxSpinnerModule
+    NgxSpinnerModule 
 
   ],
   providers: [],
